8ada7fbf1b28283185aa35ca997832eb1e97a857
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-01-21 22:23:42 -0500

    Moved app styles to App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,35 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body {
+    font-family: 'Josefin Sans';
+    background-color: #dad5d5;
+}
+
+div#app-header {
+    margin-top: 250px;
+    background-color: white;
+    padding: 40px;
+    border-radius: 8px;
+    text-transform: uppercase;
+    text-align: center;
+}
+
+div#app {
+    margin-top: 45px;
+    background-color: white;
+    padding: 40px;
+}
+
+/**
+ * Bootstrap overrides
+ */
+.navbar {
+    border-radius: 0px;
+    height: 300px;
+    background-color: #a00909;
+    border-color: transparent;
+}
+
 </style>
