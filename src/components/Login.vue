a33263fb5fa557ec594e7dc712463aeb8fe0b99a
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-01-22 01:00:58 -0500

    Added our Login controller

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="row">
+    <form action="">
+        <div class="form-group">
+            <label for="account_number">Account Number</label>
+            <input type="text" name="account_number" id="account_number" class="form-control" value="" required="required" placeholder="Account Number" v-model="username">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="" id="password" class="form-control" required="required" title="" placeholder="Password" v-model="password">
+        </div>
+
+        <button type="button" class="btn btn-info" v-on:click="login">Login</button>
+    </form>
+</div>
+</template>
+
+<script>
+import sendMessage from '../base'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      'username': null,
+      'password': null
+    }
+  },
+  methods: {
+    login: function () {
+      console.log('Logging in')
+      const msg = {
+        'messageType': 'login',
+        'params': {
+          'username': this.username,
+          'password': this.password
+        }
+      }
+      sendMessage(msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
