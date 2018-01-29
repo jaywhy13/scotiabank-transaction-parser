e3931168fde3d3535e8cae2c999ed03f61f4d594
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-01-28 23:49:45 -0500

    Writing the listener in base.js wasn't working. Had a difficulty getting the other components to listen to the emits.

--- src/base.js
+++ src/base.js
@@ -1,16 +1,17 @@
-import vue from 'vue'
-
 var API_BASE = 'ws://localhost:3000'
 var WEBSOCKET_URL = API_BASE + '/ws'
 const socket = new WebSocket(WEBSOCKET_URL)
-socket.addEventListener('message', function (event) {
-  console.log('Socket receive: ', event.data)
-  vue.$emit('socket-message', event.data)
-})
 
 export default function sendMessage (data) {
   console.log('Socket send: ', JSON.stringify(data))
   socket.send(JSON.stringify(data))
 }
 
-export { sendMessage, socket }
+function setupSocket (vm) {
+  socket.addEventListener('message', function (event) {
+    console.log('Socket receive: ', event.data)
+    vm.$emit('socketMessage', JSON.parse(event.data))
+  })
+}
+
+export { sendMessage, socket, setupSocket }
