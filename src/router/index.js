a66064b7ec584ac0d2eb0824400f679a8818faae
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-01-31 00:16:30 -0500

    Added SecurityQuestion to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import SecurityQuestion from '@/components/SecurityQuestion'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/security-question',
+      name: 'SecurityQuestion',
+      component: SecurityQuestion
     }
   ]
 })
