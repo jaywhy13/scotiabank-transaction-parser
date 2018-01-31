af69a42a9391b6bba8ef4c7274d791b33cb430e6
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-01-31 00:17:22 -0500

    Added code to handle answering of security questions

--- websocket.py
+++ websocket.py
@@ -21,16 +21,62 @@ class ScotiaBankSocketHandler(websocket.WebSocketHandler):
         print("Got message: {}".format(message))
         message = json.loads(message)
         message_type = message.get("messageType")
-        params = message.get("params")
+        params = message.get("params") or {}
         func_name = "handle_{}".format(message_type.replace("-", "_"))
         func = getattr(self, func_name)
         func(**params)
 
     def handle_login(self, **params):
+        """ Callback executed when we receive a message of type 'login'
+        """
         account_number = params.get("account_number")
         password = params.get("password")
         print("Logging in with account {}".format(account_number))
-        self.scotiabank.login(account_number, password)
+        try:
+            self.scotiabank.login(account_number, password)
+            self.send_message({
+                'messageType': 'login-success'
+            })
+        except Exception as e:
+            login_error = e.message
+            self.send_message({
+                'messageType': 'login-failed',
+                'params': {
+                    'message': login_error
+                }
+            })
+
+    def handle_request_security_question(self):
+        """ Sends the security question over the wire to the customer
+        """
+        question = self.scotiabank.get_security_question()
+        self.send_message({
+            'messageType': 'security-question',
+            'params': {
+                'security-question': question
+            }
+        })
+
+    def handle_security_question_answer(self, **params):
+        """ This is called when we receive a message of type handle answer
+            security question
+        """
+        answer = params.get("answer")
+        try:
+            self.scotiabank.answer_security_question(answer)
+            self.send_message({
+                'messageType': 'security-question-correct',
+            })
+        except Exception as e:
+            print(e)
+            error_message = e.message
+            self.send_message({
+                'messageType': 'security-question-incorrect',
+                'params': {
+                    'message': error_message
+                }
+            })
+
 
     def on_close(self):
         print("Socket closed")
