d037a8293f245175f8d24de69b54094d05d81151
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-02-14 15:32:20 -0500

    Added a basic template for the Account page

---
+++ src/components/Account.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <h2 v-text="name"></h2>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Transaction</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="transaction in transactions" v-bind:key="transaction.description">
+                    <td v-text="transaction.date">
+                    </td>
+                    <td v-text="transaction.description"></td>
+                    <td v-text="transaction.amount"></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
