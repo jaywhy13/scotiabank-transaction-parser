290c57ae4da31436b2ee844f1cc06ce6f0413533
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-02-14 15:32:51 -0500

    Make the account number linkable to the next page

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -3,12 +3,20 @@
         <table class="table table-hover">
             <thead>
                 <tr>
+                    <th></th>
                     <th>Account</th>
                     <th>Balance</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="account in accounts" v-bind:key="account.name">
+                    <td>
+                        <router-link :to="{name: 'Account', params: {branchCode: account.branch_code, accountNumber: account.account_number}}">
+                          <span v-text="account.branch_code"></span>
+                          <span v-text="account.account_number"></span>
+                        </router-link>
+                    </td>
+                    <td v-text="account.account_number"></td>
                     <td v-text="account.name"></td>
                     <td v-text="account.balance"></td>
                 </tr>
